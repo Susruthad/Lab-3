b24df901c1b0856e11396b49a15c013e1487e1b9
Susruthad <susruthanewid@gmail.com>
2024-01-29 16:30:45 -0800

    initial commit

--- Lab 3/ViewController.swift
+++ Lab 3/ViewController.swift
@@ -14,6 +14,69 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBOutlet weak var countryTextField: UITextField!
+    
+    @IBOutlet weak var ageTextField: UITextField!
+    
+    @IBOutlet weak var textViewInput: UITextView!
+    
+    @IBOutlet weak var invisibleLabel: UILabel!
+    
+    
+    @IBAction func firstNameInput(_ sender: Any) {
+    }
+    
+    @IBAction func lastNameInput(_ sender: Any) {
+    }
+    
+    @IBAction func countryInput(_ sender: Any) {
+    }
+    
+    @IBAction func ageInput(_ sender: Any) {
+    }
+    
+    @IBAction func addButton(_ sender: Any) {
+        updateTextView()
+    }
+    
+    @IBAction func submitButton(_ sender: Any) {
+        if incompleteInformation() {
+                 invisibleLabel.text = "Successfully submitted!"
+                   } else {
+                   invisibleLabel.text = "Complete the missing Info!"
+                           }
+    }
+    
+    @IBAction func clearButton(_ sender: Any) {
+        clearAllFields()
+    }
+    
+    
+    private func clearAllFields() {
+               firstNameTextField.text = ""
+               lastNameTextField.text = ""
+               countryTextField.text = ""
+               ageTextField.text = ""       
+               textViewInput.text = ""
+               invisibleLabel.text = ""
+           }
+    
+    private func updateTextView() {
+        let fullName = "Full Name: \(firstNameTextField.text ?? "") \(lastNameTextField.text ?? "")"
+        let userInfo = "\(fullName)\nCountry: \(countryTextField.text ?? "")\nAge: \(ageTextField.text ?? "")"
+        textViewInput.text = userInfo
+    }
+    
+    private func incompleteInformation() -> Bool {
+               return !firstNameTextField.text!.isEmpty &&
+                      !lastNameTextField.text!.isEmpty &&
+                      !countryTextField.text!.isEmpty &&
+                      !ageTextField.text!.isEmpty
+           }
+    
 }
 
